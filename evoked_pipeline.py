451a8a7b020389e0f79a0c1f37646546ba8655f6
Christopher J. Bailey <bailey.cj@gmail.com>
2016-01-17 19:26:09 +0100

    remove label.fill

--- evoked_pipeline.py
+++ evoked_pipeline.py
@@ -1739,8 +1739,8 @@ if do_make_FFA_functional_label_individual_from_groupavg:
                           subjects_dir=fs_subjects_dir,
                           n_jobs=4, copy=False)
 
-            # write out filled-in label
-            labels[hemi]['gavg'] = cur_lab.fill(src)
+            # use label as-is, even though it won't look good on highres surf
+            labels[hemi]['gavg'] = cur_lab
             mne.write_label(out_label_name_schema.format(hemi, func_cont),
                             labels[hemi]['gavg'])
 
